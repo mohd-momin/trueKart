05e390fae39ae4ca2951f9bed489052715226c8c
mohd-momin <mohammadmomin_se21a14_53@dtu.ac.in>
2024-07-16 00:39:35 +0530

    committed

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -6,7 +6,6 @@ import { useCart } from "../context/cart";
 import axios from "axios";
 import toast from "react-hot-toast";
 import Layout from "./../components/Layout/Layout";
-// import SliderComponent from '../components/Layout/Slider';
 import { AiOutlineReload } from "react-icons/ai";
 import "../styles/Homepage.css";
 
